25fb19fcdc76f91d4ef23eb24d857eb69b3cb21e
Shrayans <shrayansjain1409@gmail.com>
2020-04-19 22:24:04 +0530

    Integrating Server_LC and Service_LC

---
+++ Deployment_manager/Services/Algorithm_Repo/Algorithm.py
@@ -0,0 +1,10 @@
+import communication_module
+import sensor
+import sys
+
+topic = str(sys.argv[1])
+
+print('Algorithm1',topic)
+
+#for val in communication_module.Sersor_Stream('topic'):
+#	print (val)

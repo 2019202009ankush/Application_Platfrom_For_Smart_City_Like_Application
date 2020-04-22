ab5f39a15d2cfdf66d32ee0d33b1c89d880853d9
Ankush <ankush.mitra@students.iiit.ac.in>
2020-04-23 00:10:40 +0530

    modified sensor up

--- sensor/sensor_up.py
+++ sensor/sensor_up.py
@@ -1,2 +1,4 @@
 import sensor
 sensor.sensor('temp1','temperature','obh_112','0.0.0.0','9557')
+sensor.sensor('d1','numeric_attandance','obh_112','0.0.0.0','9557')
+sensor.sensor('d1','doorstep','obh_112','0.0.0.0','9557')

004bf1d8d5adcb30b579ba2f3b84a62903eab7f2
Ankush Mitra <54775848+2019202009ankush@users.noreply.github.com>
2020-04-23 16:17:29 +0530

    Added new 10 sensor

--- sensor/num_up.py
+++ sensor/num_up.py
@@ -1,2 +1,24 @@
 import sensor
-sensor.sensor('d1','numeric_attandance','obh_112','0.0.0.0','9557')
+
+import thread
+th1 = threading.Thread(target=sensor.sensor,kwargs={'d1':id,'numeric_attandance':typ,'obh_112':loc,'0.0.0.0':ip,'9557':port})
+th2 = threading.Thread(target=sensor.sensor,kwargs={'d2':id,'numeric_attandance':typ,'obh_112':loc,'0.0.0.0':ip,'9557':port})
+th3 = threading.Thread(target=sensor.sensor,kwargs={'d3':id,'numeric_attandance':typ,'obh_112':loc,'0.0.0.0':ip,'9557':port})
+th4 = threading.Thread(target=sensor.sensor,kwargs={'d4':id,'numeric_attandance':typ,'obh_112':loc,'0.0.0.0':ip,'9557':port})
+th5 = threading.Thread(target=sensor.sensor,kwargs={'d5':id,'numeric_attandance':typ,'obh_112':loc,'0.0.0.0':ip,'9557':port})
+th6 = threading.Thread(target=sensor.sensor,kwargs={'d6':id,'numeric_attandance':typ,'obh_112':loc,'0.0.0.0':ip,'9557':port})
+th7 = threading.Thread(target=sensor.sensor,kwargs={'d7':id,'numeric_attandance':typ,'obh_112':loc,'0.0.0.0':ip,'9557':port})
+th8 = threading.Thread(target=sensor.sensor,kwargs={'d8':id,'numeric_attandance':typ,'obh_112':loc,'0.0.0.0':ip,'9557':port})
+th9 = threading.Thread(target=sensor.sensor,kwargs={'d9':id,'numeric_attandance':typ,'obh_112':loc,'0.0.0.0':ip,'9557':port})
+th10 = threading.Thread(target=sensor.sensor,kwargs={'d10':id,'numeric_attandance':typ,'obh_112':loc,'0.0.0.0':ip,'9557':port})
+# sensor.sensor('d1','numeric_attandance','obh_112','0.0.0.0','9557')
+th1.start
+th2.start
+th3.start
+th4.start
+th5.start
+th6.start
+th7.start
+th8.start
+th9.start
+th10.start

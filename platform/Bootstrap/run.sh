2dca7dd0a2ebf009267ed7b452057412049ed805
Ankush Mitra <ankush.mitra@students.iiit.ac.in>
2020-05-05 22:09:42 +0530

    added support for topic data visualization

--- platform/Bootstrap/run.sh
+++ platform/Bootstrap/run.sh
@@ -6,6 +6,7 @@ sudo docker run --rm --network=host -v $PWD/../../:/opt dock_sensor &
 sudo docker run --rm --network=host -v $PWD/../../:/opt dock_server &
 sudo docker run --rm --network=host -v $PWD/../../:/opt dock_service &
 sudo docker run --rm --network=host -v $PWD/../../:/opt dock_topo &
+sudo docker run --rm --network=host lensesio/fast-data-dev
 gnome-terminal --  bash -c "sudo docker run --rm --network=host -v $PWD/../../:/opt dock_action; exec bash"
 gnome-terminal --  bash -c "sudo docker run --rm --network=host -v $PWD/../../:/opt dock_server1; exec bash"
 gnome-terminal --  bash -c "sudo docker run --rm --network=host -v $PWD/../../:/opt dock_server2; exec bash"
